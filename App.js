15845838c00552c7f13ffae904d85c7b523348fb
fefe49 <felixbeuzelin@gmail.com>
2024-06-11 22:10:17 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
+//Import
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FelixScreen1 from "./src/screens/FelixScreen1";
+
+const FelixScreen2 = () =>{
+  return (
+    <View>
+      <Text>FelixScreen2</Text>
+    </View>
+  )
+}
+
+const NilsScreen1 = () =>{
+  return (
+    <View>
+      <Text>NilsScreen1</Text>
+    </View>
+  )
+}
+
+const NilsScreen2 = () =>{
+  return (
+    <View>
+      <Text>NilsScreen2</Text>
+    </View>
+  )
+} 
+
+//NAVIGATORS
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Felix'>
+          {() => (
+            <Stack.Navigator>
+              <Stack.Screen name='FelixScreen1' component={FelixScreen1} options={{ title: 'Félix 1'}}/>
+              <Stack.Screen name='FelixScreen2' component={FelixScreen2} options={{ title: 'Felix 2'}}/>
+            </Stack.Navigator>
+            )}
+        </Tab.Screen>
+        <Tab.Screen name='Nils'>
+          {() => (
+            <Stack.Navigator>
+              <Stack.Screen name='NilsScreen1'component={NilsScreen1} options={{ title: 'Nils 1'}}/>
+              <Stack.Screen name='NilsScreen2'component={NilsScreen2} options={{ title: 'Nils 2'}}/>
+            </Stack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/FelixScreen1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Switch } from "react-native-gesture-handler";
+
+const FelixScreen1 = () => {
+    return (
+        <View>
+            <Text>Felix 1 record 3x3 : 26.84 secondes</Text>
+            <Text>Felix 1 record 4x4 : 3:04.34 secondes</Text>
+        </View>
+    )
+}
+export default FelixScreen1;
