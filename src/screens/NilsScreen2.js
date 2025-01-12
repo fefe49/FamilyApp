74c031145b0f929583a29ba0d034793736316a5c
fefe49 <151369176+fefe49@users.noreply.github.com>
2025-01-12 21:49:15 +0100

    Update NilsScreen2.js

--- src/screens/NilsScreen2.js
+++ src/screens/NilsScreen2.js
@@ -1,14 +1,101 @@
+import React, {useState} from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Platform,
+    TextInput,
+    TouchableOpacity,
+    Keyboard
+} from "react-native";
+import Task from "../components/Task";
 
+const NilsScreen2 = () =>{
+    const [task, setTasks] = useState();
+    const [taskItems, setTaskItems] = useState([]);
 
-import React from "react";
-import { View, Text } from "react-native";
-import { Switch } from "react-native-gesture-handler";
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTasks(null)
+    }
 
-const NilsScreen1 = () => {
+    const completedTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy);
+    }
+
     return (
         <View>
-            <Text>Pizza</Text>
+            <View style={styles.tasksWrapper}>
+                <View style={styles.items}>
+                    {
+                        taskItems.map((item, index) => {
+                            return (
+                                <TouchableOpacity onPress={() => completedTask(index)}>
+                                    <Task key={index} text={item} />
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+
+                </View>
+            </View>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.writeTask}>
+                <TextInput style={styles.input} placeholder={'écrit une tâche'} value={task} onChangeText={text => setTasks(text)} />
+            </KeyboardAvoidingView>
+            <TouchableOpacity onPress={handleAddTask}>
+                <View style={styles.addWrapper}>
+                    <Text style={styles.addText}>+</Text>
+                </View>
+            </TouchableOpacity>
         </View>
+
     )
 }
-export default NilsScreen1;
+
+const styles = StyleSheet.create({
+    item: {
+        marginTop: 30,
+    },
+    writeTask: {
+        position: 'absolute',
+        bottom: -65,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        right: 60,
+    },
+    input: {
+        paddingTop: 15,
+        paddingBottom: 15,
+        backgroundColor: '#fff',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        width: 250,
+        textAlign: 'left',
+        paddingLeft: 15,
+    },
+    addWrapper: {
+        position: 'absolute',
+        bottom: -65
+        ,
+        width: 60,
+        height: 60,
+        backgroundColor: '#fff',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        right: 50,
+    },
+    addText: {},
+
+});
+
+export default NilsScreen2;
