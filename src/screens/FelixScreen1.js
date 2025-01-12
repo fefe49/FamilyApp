a972a36d6d1402304e0445ca7df5d4d0424b2413
fefe49 <151369176+fefe49@users.noreply.github.com>
2025-01-12 21:47:40 +0100

    Update FelixScreen1.js

--- src/screens/FelixScreen1.js
+++ src/screens/FelixScreen1.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/core";
-import { Switch } from "react-native-gesture-handler";
 
 const FelixScreen1 = () => {
-    const navigation = useNavigation ();
+    const navigation = useNavigation();
     return (
         <View>
-            <TouchableOpacity onPress={() => {navigation.navigate("FelixScreen2")}}>
+            <TouchableOpacity onPress={() => { navigation.navigate("FelixScreen2") }}>
                 <Text>4x4</Text>
             </TouchableOpacity>
-            <Text>Felix record 3x3 : 23.86 secondes</Text>
-
+            <Text>Felix record 3x3 : 13.707 secondes</Text>
+            <Text>Felix record 4x4 : 1:34.11 secondes</Text>
         </View>
     )
 }
